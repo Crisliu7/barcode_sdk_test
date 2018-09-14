d4d99c9da76634d65111ee747598a39a0c6f480e
Crisliu7 <zliu@6river.com>
2018-09-14 18:07:56 -0400

    feat: add zbar code, finish zbar and dynamsoft, leave scandit to be done

--- dynamsoft/dynamsoft.cpp
+++ dynamsoft/dynamsoft.cpp
@@ -6,28 +6,34 @@ The sample demonstrates how to read barcode from the Linux Command Line.
 */
 
 #include <dirent.h>
-#include <string.h>
+#include <sys/time.h>
 #include <cstdio>
 #include <cstdlib>
 #include <cstdint>
-#include <sys/time.h>
-#include <SDL2/SDL_image.h>
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+
 #include "include/DynamsoftBarcodeReader.h"
 
+// valid image extension
 static char const * const ENABLED_FILE_EXTENSIONS[] = {
-    "png",
-    "jpg",
-    "jpeg",
-    "tif",
-    "bmp",
-    NULL
+  "png",
+  "jpg",
+  "jpeg",
+  "tif",
+  "bmp",
+  NULL
 };
 
+// image container
 typedef struct InputImage {
     char const *file_name;
     struct InputImage const *next;
 } InputImage;
 
+/**
+ * check if image extension is valid
+ */
 static int has_valid_extension(char const *file_name)
 {
   char const * const *extension = ENABLED_FILE_EXTENSIONS;
@@ -36,21 +42,25 @@ static int has_valid_extension(char const *file_name)
     const size_t file_name_length = strlen(file_name);
     const size_t extension_length = strlen(*extension);
 
-    char const * const file_extension = file_name + (file_name_length - extension_length);
+    char const * const file_extension =
+            file_name + (file_name_length - extension_length);
     if (file_name_length >= extension_length && strcmp(file_extension, *extension) == 0) 
     {
       return true;
     }
   }
-  return true;
+
+  return false;
 }
 
-static InputImage *push_if_valid_image(char const * filename,
-                                      InputImage *container)
+/**
+ * push images to image container
+ */
+static InputImage *push_if_valid_image(char const * filename, InputImage *container)
 {
   if (has_valid_extension(filename) == true) 
   {
-    InputImage *new_image = (InputImage *)malloc(sizeof(InputImage));
+    InputImage *new_image = (InputImage*)malloc(sizeof(InputImage));
     const size_t target_size = strlen(filename) + 1;
     new_image->file_name = (char*)malloc(target_size);
     strncpy((char*)new_image->file_name, filename, target_size);
@@ -63,7 +73,9 @@ static InputImage *push_if_valid_image(char const * filename,
   return container;
 }
 
-
+/**
+ * load images from file or folder
+ */
 InputImage const *get_input_files(int argc, char const *argv[])
 {
   InputImage *ret = NULL;
@@ -72,7 +84,6 @@ InputImage const *get_input_files(int argc, char const *argv[])
   for (int arg_idx = 1; arg_idx < argc; ++arg_idx) 
   {
     char const * const current_arg = argv[arg_idx];
-
     DIR *dir;
     struct dirent *ent;
     if ((dir = opendir(current_arg)) != NULL) 
@@ -80,8 +91,7 @@ InputImage const *get_input_files(int argc, char const *argv[])
       // We have a directory
       while ((ent = readdir (dir)) != NULL) 
       {
-        char *combined_name = (char*)malloc(sizeof(current_arg) +
-                                      sizeof(ent->d_name) + 2);
+        char *combined_name = (char*)malloc(sizeof(current_arg) + sizeof(ent->d_name) + 2);
         combined_name[0] = '\0';
         strcat(combined_name, current_arg);
         strcat(combined_name, "/");
@@ -90,9 +100,7 @@ InputImage const *get_input_files(int argc, char const *argv[])
         ret = push_if_valid_image(combined_name, ret);
       }
       closedir (dir);
-    } 
-    else 
-    {
+    } else {
       // We have a file
       ret = push_if_valid_image(current_arg, ret);
     }
@@ -102,38 +110,30 @@ InputImage const *get_input_files(int argc, char const *argv[])
 }
 
 /**
- * Helper function to load image from disk using SDL2
+ * opencv read single image
  */
-static bool load_image(const char* image_name, uint8_t** data,
-                         uint32_t* width, uint32_t *height,
-                         uint32_t* row_stride)
+static bool read_image(const char* image_name, uint8_t **data, uint32_t *width, uint32_t *height, uint32_t *row_stride)
 {
-  SDL_Surface *image = IMG_Load(image_name);
-  if (image == NULL) {
-      printf("IMG_Load '%s' failed: %s\n", image_name, IMG_GetError());
-      return false;
-  }
 
-  SDL_Surface* image_rgb = SDL_ConvertSurfaceFormat(image,
-                                                    SDL_PIXELFORMAT_RGB24,
-                                                    0);
-  SDL_FreeSurface(image);
-  if (image_rgb == NULL) {
-      printf("Image '%s' convertion failed: %s\n", image_name, IMG_GetError());
-      return false;
+  cv::Mat image;
+  const std::string image_name_str(image_name);
+  image = cv::imread(image_name_str, cv::IMREAD_GRAYSCALE);
+  if(image.empty())               
+  {
+    std::cout <<  "OpenCV could not open or find the image" << std::endl ;
+    return false;
   }
+  // *data = image.data;
+  *width = image.cols;
+  *height = image.rows;
+  *row_stride = image.step[0];
 
-  *width = image_rgb->w;
-  *height = image_rgb->h;
-  *row_stride = image_rgb->pitch;
   const int blob_size = *row_stride * *height;
 
-  printf("Image '%s' size: %ux%u, stride %u (%u bytes)\n", image_name, *width, *height,
-          *row_stride, blob_size);
   *data = (uint8_t *)malloc(blob_size);
-  memcpy(*data, image_rgb->pixels, blob_size);
+  memcpy(*data, image.data, blob_size);
 
-  SDL_FreeSurface(image_rgb);
+  std::cout<< "Image: "<< image_name << ", size: " << *width << " * " << *height << ", stride " << *row_stride << "("<< blob_size << " bytes)" << std::endl;
 
   return true;
 }
@@ -141,60 +141,22 @@ static bool load_image(const char* image_name, uint8_t** data,
 
 
 
-void OutputResult(CBarcodeReader& reader,int errorcode,float time,char*pszTextResult,int pszTextResultLength)
-{
-	char * pszTemp1 = NULL;
-	char* pszTempResult = NULL;
-	int iRet = errorcode;
-
-	if (iRet != DBR_OK && iRet != DBRERR_LICENSE_EXPIRED && iRet != DBRERR_QR_LICENSE_INVALID &&
-		iRet != DBRERR_1D_LICENSE_INVALID && iRet != DBRERR_PDF417_LICENSE_INVALID && iRet != DBRERR_DATAMATRIX_LICENSE_INVALID && iRet != DBRERR_AZTEC_LICENSE_INVALID)
-	{
-		snprintf(pszTextResult, pszTextResultLength, "Failed to read barcode: %s\r\n", CBarcodeReader::GetErrorString(iRet));
-		return;
-	}
-
-	STextResultArray *paryResult = NULL;
-	reader.GetAllTextResults(&paryResult);
-
-	if (paryResult->nResultsCount == 0)
-	{
-		snprintf(pszTextResult, pszTextResultLength, "No barcode found. Total time spent: %.3f seconds.\r\n", time);
-		CBarcodeReader::FreeTextResults(&paryResult);
-		return;
-	}
-
-	snprintf(pszTextResult, pszTextResultLength, "Total barcode(s) found: %d. Total time spent: %.3f seconds\r\n\r\n", paryResult->nResultsCount, time);
-	for (int iIndex = 0; iIndex < paryResult->nResultsCount; iIndex++)
-	{
-		pszTempResult = (char*)malloc(4096);
-		snprintf(pszTempResult, 2048, "Barcode %d:\r\n    Type: %s\r\n    Value: %s\r\n", iIndex + 1, paryResult->ppResults[iIndex]->pszBarcodeFormatString, paryResult->ppResults[iIndex]->pszBarcodeText);
-
-		strcat(pszTextResult,pszTempResult);
-		free(pszTempResult);
-		free(pszTemp1);
-	}
-
-	CBarcodeReader::FreeTextResults(&paryResult);
-	return;
-}
-
 int main(int argc, const char* argv[])
 {
   int iRet = -1;
-	int iExitFlag = 0;
 	char szErrorMsg[256];
 	float fCostTime = 0;
 
+  if (argc < 2) 
+	{
+		printf("Usage: ReadBarcode [ImageFilePath]\n");
+		return -1; 
+	}
 
 	CBarcodeReader reader;
 	reader.InitLicense("t0068NQAAAFjNxhQOFJImz3ElX+DrOBwwfNaQP3KXFlzWlGtuNW5B2oy6uPJA1TB6o3hxT9qOeEkhIXeG19lt5VVLTENk41s=");
 
-	if(argc<=1)
-	{
-		printf("Usage: ReadBarcode [ImageFilePath]\n");
-		return 1; 
-	}
+
   // load image file/folder
   InputImage const * const images = get_input_files(argc, argv);
   uint8_t *image_data = NULL;
@@ -204,7 +166,7 @@ int main(int argc, const char* argv[])
   for (InputImage const *current_image = images; current_image != NULL; current_image = current_image->next)
   {
     uint32_t image_width, image_height, row_stride;
-    if (load_image(current_image->file_name, &image_data, &image_width, &image_height, &row_stride) == false)
+    if (read_image(current_image->file_name, &image_data, &image_width, &image_height, &row_stride) == false)
     {
         printf("Failed to load image '%s'.\n", current_image->file_name);
     }
@@ -213,24 +175,89 @@ int main(int argc, const char* argv[])
 	  struct timeval ullTimeBegin, ullTimeEnd;
     gettimeofday(&ullTimeBegin, NULL);    
     // iRet = reader.DecodeFile(current_image->file_name);
-    iRet = reader.DecodeBuffer(image_data, image_width, image_height, row_stride, IPF_RGB_888, "");
+    // iRet = reader.DecodeBuffer(image_data, image_width, image_height, row_stride, IPF_RGB_888, "");
+    iRet = reader.DecodeBuffer(image_data, image_width, image_height, row_stride, IPF_GrayScaled, "");
     gettimeofday(&ullTimeEnd, NULL);
     fCostTime = (float)((ullTimeEnd.tv_sec * 1000 * 1000 +  ullTimeEnd.tv_usec) - (ullTimeBegin.tv_sec * 1000 * 1000 + ullTimeBegin.tv_usec))/(1000 * 1000);
+
 
     if(iRet !=DBR_OK)
     {
       printf("Error code: %d. Error message: %s\n", iRet, szErrorMsg);
+      return 0;
     }
 
-    // output barcode result.
-    char * pszTextResult = NULL;
-    pszTextResult = (char*)malloc(8192);
-    OutputResult(reader, iRet, fCostTime, pszTextResult, 8192);
-    printf(pszTextResult);
-    free(pszTextResult);
+    // output decoding result.
+    if (iRet != DBR_OK && iRet != DBRERR_LICENSE_EXPIRED && iRet != DBRERR_QR_LICENSE_INVALID &&
+      iRet != DBRERR_1D_LICENSE_INVALID && iRet != DBRERR_PDF417_LICENSE_INVALID && iRet != DBRERR_DATAMATRIX_LICENSE_INVALID && iRet != DBRERR_AZTEC_LICENSE_INVALID)
+    {
+      std::cout << "Failed to read barcode: \r\n" << CBarcodeReader::GetErrorString(iRet) << std::endl;
+      return 0;
+    }
 
+    STextResultArray *paryResult = NULL;
+    reader.GetAllTextResults(&paryResult);
+
+    if (paryResult->nResultsCount == 0)
+    {
+      std::cout << "    No barcode found. Total time spent: " << fCostTime << " seconds."<< std::endl;
+    }
+    else
+    {
+      std::cout << "    Total barcode(s) found: " << paryResult->nResultsCount << ". Total time spent: " << fCostTime << std::endl;
+    }
+    for (int iIndex = 0; iIndex < paryResult->nResultsCount; iIndex++)
+    {
+      std::cout << "        Barcode " << iIndex + 1 << ":" << std::endl;
+      std::cout << "            Type " << paryResult->ppResults[iIndex]->pszBarcodeFormatString << std::endl;
+      std::cout << "            Value " << paryResult->ppResults[iIndex]->pszBarcodeText << std::endl;
+    }
+
+    CBarcodeReader::FreeTextResults(&paryResult);
 
   }
 
  	return 0;
 }
+
+
+
+
+
+
+/**
+ * Helper function to load image from disk using SDL2
+ */
+// static bool read_image(const char* image_name, uint8_t** data,
+//                          uint32_t* width, uint32_t *height,
+//                          uint32_t* row_stride)
+// {
+//   SDL_Surface *image = IMG_Load(image_name);
+//   if (image == NULL) {
+//       printf("IMG_Load '%s' failed: %s\n", image_name, IMG_GetError());
+//       return false;
+//   }
+
+//   SDL_Surface* image_rgb = SDL_ConvertSurfaceFormat(image,
+//                                                     SDL_PIXELFORMAT_RGB24,
+//                                                     0);
+//   SDL_FreeSurface(image);
+//   if (image_rgb == NULL) {
+//       printf("Image '%s' convertion failed: %s\n", image_name, IMG_GetError());
+//       return false;
+//   }
+
+//   *width = image_rgb->w;
+//   *height = image_rgb->h;
+//   *row_stride = image_rgb->pitch;
+//   const int blob_size = *row_stride * *height;
+
+//   printf("Image '%s' size: %ux%u, stride %u (%u bytes)\n", image_name, *width, *height,
+//           *row_stride, blob_size);
+//   *data = (uint8_t *)malloc(blob_size);
+//   memcpy(*data, image_rgb->pixels, blob_size);
+
+//   SDL_FreeSurface(image_rgb);
+
+//   return true;
+// }
--- zbar/zbar.cpp
+++ zbar/zbar.cpp
@@ -1,53 +1,249 @@
+
+// #define STR(s) #s
+
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstdint>
+#include <cstring>
 #include <iostream>
+// #include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+// #include <opencv2/highgui/highgui.hpp>
+#include <dirent.h>
+#include <sys/time.h>
 #include <zbar.h>
-#define STR(s) #s
 
-using namespace std;
-using namespace zbar;
 
-int main (int argc, char **argv)
+
+// valid image extension
+static char const * const ENABLED_FILE_EXTENSIONS[] = {
+  "png",
+  "jpg",
+  "jpeg",
+  "tif",
+  "bmp",
+  NULL
+};
+
+// image container
+typedef struct InputImage {
+    char const *file_name;
+    struct InputImage const *next;
+} InputImage;
+
+/**
+ * check if image extension is valid
+ */
+static int has_valid_extension(char const *file_name)
 {
-    if(argc < 2) return(1);
+  char const * const *extension = ENABLED_FILE_EXTENSIONS;
+  for (; *extension != NULL; ++extension) 
+  {
+    const size_t file_name_length = strlen(file_name);
+    const size_t extension_length = strlen(*extension);
 
-#ifdef MAGICK_HOME
-    // http://www.imagemagick.org/Magick++/
-    //    under Windows it is necessary to initialize the ImageMagick
-    //    library prior to using the Magick++ library
-    Magick::InitializeMagick(MAGICK_HOME);
-#endif
+    char const * const file_extension =
+            file_name + (file_name_length - extension_length);
+    if (file_name_length >= extension_length && strcmp(file_extension, *extension) == 0) 
+    {
+      return true;
+    }
+  }
 
-    // create a reader
-    ImageScanner scanner;
+  return false;
+}
 
-    // configure the reader
-    scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);
+/**
+ * push images to image container
+ */
+static InputImage *push_if_valid_image(char const * filename, InputImage *container)
+{
+  if (has_valid_extension(filename) == true) 
+  {
+    InputImage *new_image = (InputImage*)malloc(sizeof(InputImage));
+    const size_t target_size = strlen(filename) + 1;
+    new_image->file_name = (char*)malloc(target_size);
+    strncpy((char*)new_image->file_name, filename, target_size);
 
-    // obtain image data
-    Magick::Image magick(argv[1]);  // read an image file
-    int width = magick.columns();   // extract dimensions
-    int height = magick.rows();
-    Magick::Blob blob;              // extract the raw data
-    magick.modifyImage();
-    magick.write(&blob, "GRAY", 8);
-    const void *raw = blob.data();
+    new_image->next = container;
 
+    container = new_image;
+  }
+
+  return container;
+}
+
+/**
+ * load images from file or folder
+ */
+InputImage const *get_input_files(int argc, char const *argv[])
+{
+  InputImage *ret = NULL;
+
+  // We skip the first argument
+  for (int arg_idx = 1; arg_idx < argc; ++arg_idx) 
+  {
+    char const * const current_arg = argv[arg_idx];
+    DIR *dir;
+    struct dirent *ent;
+    if ((dir = opendir(current_arg)) != NULL) 
+    {
+      // We have a directory
+      while ((ent = readdir (dir)) != NULL) 
+      {
+        char *combined_name = (char*)malloc(sizeof(current_arg) + sizeof(ent->d_name) + 2);
+        combined_name[0] = '\0';
+        strcat(combined_name, current_arg);
+        strcat(combined_name, "/");
+        strcat(combined_name, ent->d_name);
+
+        ret = push_if_valid_image(combined_name, ret);
+      }
+      closedir (dir);
+    } else {
+      // We have a file
+      ret = push_if_valid_image(current_arg, ret);
+    }
+  }
+
+  return ret;
+}
+
+/**
+ * opencv read single image
+ */
+static bool read_image(const char* image_name, uint8_t **data, uint32_t *width, uint32_t *height, uint32_t *row_stride)
+{
+
+  cv::Mat image;
+  const std::string image_name_str(image_name);
+  image = cv::imread(image_name_str, cv::IMREAD_GRAYSCALE);
+  if(image.empty())               
+  {
+    std::cout <<  "OpenCV could not open or find the image" << std::endl ;
+    return false;
+  }
+  // *data = image.data;
+  *width = image.cols;
+  *height = image.rows;
+  *row_stride = image.step[0];
+
+  const int blob_size = *row_stride * *height;
+
+  *data = (uint8_t *)malloc(blob_size);
+  memcpy(*data, image.data, blob_size);
+
+  std::cout<< "Image: "<< image_name << ", size: " << *width << " * " << *height << ", stride " << *row_stride << "("<< blob_size << " bytes)" << std::endl;
+
+
+  return true;
+}
+
+
+
+int main(int argc, const char* argv[])
+{
+	if(argc <= 1)
+	{
+		printf("Usage: ReadBarcode [ImageFilePath]\n");
+		return 0; 
+	}
+
+  // create a reader
+  zbar::ImageScanner scanner;
+
+  // configure the reader
+  scanner.set_config(zbar::ZBAR_NONE, zbar::ZBAR_CFG_ENABLE, 1);
+
+  // load image file/folder
+  InputImage const * const images = get_input_files(argc, argv);
+  uint8_t *image_data = NULL;
+  float fCostTime = 0;
+
+  // traverse loaded image
+  for (InputImage const *current_image = images; current_image != NULL; current_image = current_image->next)
+  {
+    // uint32_t image_width = 1, image_height = 1, row_stride = 1;
+    uint32_t image_width, image_height, row_stride;
+    
+    // if (read_image(current_image->file_name, &image_data, &image_width, &image_height, &row_stride) == false)
+    if (read_image(current_image->file_name, &image_data, &image_width, &image_height, &row_stride) == false)
+    {
+      printf("Failed to load image '%s'.\n", current_image->file_name);
+    }
+    
+    // decode and calculate time cost
+	  struct timeval ullTimeBegin, ullTimeEnd;
+    gettimeofday(&ullTimeBegin, NULL);    
     // wrap image data
-    Image image(width, height, "GREY", raw, width * height);
-
+    zbar::Image image(image_width, image_height, "GREY", image_data, row_stride);
     // scan the image for barcodes
     int n = scanner.scan(image);
+    gettimeofday(&ullTimeEnd, NULL);
+    fCostTime = (float)((ullTimeEnd.tv_sec * 1000 * 1000 +  ullTimeEnd.tv_usec) - (ullTimeBegin.tv_sec * 1000 * 1000 + ullTimeBegin.tv_usec))/(1000 * 1000);
 
-    // extract results
-    for(Image::SymbolIterator symbol = image.symbol_begin();
-        symbol != image.symbol_end();
-        ++symbol) {
-        // do something useful with results
-        cout << "decoded " << symbol->get_type_name()
-             << " symbol \"" << symbol->get_data() << '"' << endl;
+
+
+    if (image.symbol_begin() == image.symbol_end()) 
+    {
+      std::cout << "    No barcode found. Total time spent: " << fCostTime << " seconds."<< std::endl;
+    }
+    else{
+      std::cout << "    Total barcode(s) found: " << n << ". Total time spent: " << fCostTime << std::endl;
+    }
+    // output decoding results
+    int iIndex = 0;
+    for(zbar::Image::SymbolIterator symbol = image.symbol_begin(); symbol != image.symbol_end(); ++symbol) 
+    {
+      std::cout << "        Barcode " << iIndex + 1 << ":" << std::endl;
+      std::cout << "            Type: " << symbol->get_type_name() << std::endl;
+      std::cout << "            Value: " << symbol->get_data() << std::endl;
+      iIndex++;
     }
 
     // clean up
     image.set_data(NULL, 0);
 
-    return(0);
+  }
 }
+
+
+
+
+/**
+ * Helper function to load image from disk using SDL2
+ */
+// static bool read_image(const char* image_name, uint8_t** data,
+//                          uint32_t* width, uint32_t *height,
+//                          uint32_t* row_stride)
+// {
+//   SDL_Surface *image = IMG_Load(image_name);
+//   if (image == NULL) {
+//       printf("IMG_Load '%s' failed: %s\n", image_name, IMG_GetError());
+//       return false;
+//   }
+
+//   SDL_Surface* image_rgb = SDL_ConvertSurfaceFormat(image,
+//                                                     SDL_PIXELFORMAT_RGB24,
+//                                                     0);
+//   SDL_FreeSurface(image);
+//   if (image_rgb == NULL) {
+//       printf("Image '%s' convertion failed: %s\n", image_name, IMG_GetError());
+//       return false;
+//   }
+
+//   *width = image_rgb->w;
+//   *height = image_rgb->h;
+//   *row_stride = image_rgb->pitch;
+//   const int blob_size = *row_stride * *height;
+
+//   printf("Image '%s' size: %ux%u, stride %u (%u bytes)\n", image_name, *width, *height,
+//           *row_stride, blob_size);
+//   *data = (uint8_t *)malloc(blob_size);
+//   memcpy(*data, image_rgb->pixels, blob_size);
+
+//   SDL_FreeSurface(image_rgb);
+
+//   return true;
+// }
